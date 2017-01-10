5442de8a43258f20afd56b1846f0c3487d1d9204
beanstown106 <nbenis106@gmail.com>
2017-01-10 05:01:00 +0000

    Custom ambient display settings (2/2)
    [neo: This is revival of old code.]
    
    AICP'fy: moved to settings and adapted to aicp base
    
    Change-Id: I9837809216d499da059ad42f94bb90e311aeb26e
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	res/values/screwd_arrays.xml
    	res/values/screwd_strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -168,4 +168,5 @@ public class Settings extends SettingsActivity {
     public static class SystemSettings extends SettingsActivity { /* empty */ }
 
     public static class ButtonsSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class AmbientSettingsActivity extends SettingsActivity { /* empty */ }
 }
