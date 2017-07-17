0d5ceee3e82a2c2f30ef4cb8cf5e5ca027613985
Ahmed Hady <ahmedhady6@gmail.com>
2017-07-17 18:50:58 +0200

    Revert "settings: Add buttons customization fragment"
    This reverts commit ac05d3d1e01d04be60decd1c7a4609625f3fdbef.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -166,7 +166,5 @@ public class Settings extends SettingsActivity {
     public static class DeviceSettings extends SettingsActivity { /* empty */ }
     public static class PersonalSettings extends SettingsActivity { /* empty */ }
     public static class SystemSettings extends SettingsActivity { /* empty */ }
-
-    public static class ButtonsSettingsActivity extends SettingsActivity { /* empty */ }
     public static class AmbientSettingsActivity extends SettingsActivity { /* empty */ }
 }
--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -262,7 +262,6 @@ public class SettingsActivity extends SettingsDrawerActivity
             Settings.Lte4GEnableActivity.class.getName(),
             Settings.WirelessSettingsActivity.class.getName(),
             //device_section
-            Settings.ButtonsSettingsActivity.class.getName(),
             Settings.HomeSettingsActivity.class.getName(),
             Settings.SoundSettingsActivity.class.getName(),
             Settings.DisplaySettingsActivity.class.getName(),
@@ -386,7 +385,6 @@ public class SettingsActivity extends SettingsDrawerActivity
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
-            ButtonsSettings.class.getName(),
             AmbientSettings.class.getName()
     };
 
