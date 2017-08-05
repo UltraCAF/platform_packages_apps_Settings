b9c0633eda281086181aa76fbd307028ca47ed5e
Ahmed Hady <ahmedhady6@gmail.com>
2017-08-05 12:29:12 +0200

    (1/2) Moving Buttons to UltraManager

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -264,7 +264,6 @@ public class SettingsActivity extends SettingsDrawerActivity
             Settings.Lte4GEnableActivity.class.getName(),
             Settings.WirelessSettingsActivity.class.getName(),
             //device_section
-            Settings.ButtonsSettingsActivity.class.getName(),
             Settings.HomeSettingsActivity.class.getName(),
             Settings.SoundSettingsActivity.class.getName(),
             Settings.DisplaySettingsActivity.class.getName(),
@@ -389,7 +388,6 @@ public class SettingsActivity extends SettingsDrawerActivity
             ManageDomainUrls.class.getName(),
             AutomaticStorageManagerSettings.class.getName(),
             AmbientSettings.class.getName(),
-            ButtonsSettings.class.getName(),
             UltraMan.class.getName()
     };
 
